20c3551d863d603725259ca66900657411552d2e
0xBlockchain <buckoscarengr@gmail.com>
2022-05-05 04:07:52 -0400

    add terraswap contracts info

--- index.ts
+++ index.ts
@@ -1,6 +1,15 @@
 import axios from 'axios';
 import { Coins, LCDClient, MnemonicKey } from '@terra-money/terra.js';
 
+//Bombay testnet
+const terraswap_token_code_id = 148;
+const terraswap_factory_code_id = 154;
+const terraswap_pair_code_id = 155;
+const terraswap_router_code_id = 63814;
+
+const terraswap_factory_contract_address = "terra18qpjm4zkvqnpjpw0zn0tdr8gdzvt8au35v45xf";
+const terraswap_router_contract_address = "terra1c58wrdkyc0ynvvxcv834kz65nfsxmw2w0pwusq";
+
 (async () => {
     const gasPrices = await axios.get<any>('https://bombay-fcd.terra.dev/v1/txs/gas_prices');
     const gasPricesCoins = new Coins(gasPrices.data); 
